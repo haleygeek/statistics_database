6b56691efa155584fc80276cdba8567d3260bdaf
Haley Speed <haleygeek@users.noreply.github.com>
2017-07-05 23:18:05 -0500

    Update printfv.php

--- printfv.php
+++ printfv.php
@@ -5,14 +5,13 @@
 <body>
 <?php
 
-
 //----------------------------------------------------------------------------------------
 //Connection info
 //----------------------------------------------------------------------------------------
 $dbhost = "localhost";
-$dbuser = "username";		//Edit to make it work
-$dbpass = "password";		//Edit to make it work
-$dbname = "db_stats";		//Edit to make it work
+$dbuser = "replace";
+$dbpass = "replace";
+$dbname = "replace";
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 
 //----------------------------------------------------------------------------------------
@@ -20,8 +19,9 @@ $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
 //----------------------------------------------------------------------------------------
 if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$experiment = "I/O Curve Fiber Volley";
-	$groups_tested = $_POST["groups_tested"];
+	$groups_tested = $_POST["groups_tested_fv"];
 }
+
 
 //----------------------------------------------------------------------------------------
 //Convert to groups
@@ -43,6 +43,12 @@ switch($groups_tested){
 	case 'oil_igf_cpf_igf':
 		$group_1 = 'oil_igf';
 		$group_2 = 'cpf_igf';
+	case 'oil_igf_cpf_saline':
+		$group_1 = 'oil_igf';
+		$group_2 = 'cpf_saline';
+	case 'oil_saline_cpf_igf':
+		$group_1 = 'oil_saline';
+		$group_2 = 'cpf_igf';
 }
 
 //----------------------------------------------------------------------------------------
@@ -57,7 +63,7 @@ $result = mysqli_query($connection, $query);
 function formatTestType ($test_type){
 	switch($test_type){
 	case 'two_way_rm_anova':
-		Return "Two-Way rmANOVA";
+		Return "Two-Way ANOVA with Repeated Measures";
 		break;
 	case 'one_way_anova':
 		Return "One-Way ANOVA";
@@ -66,7 +72,7 @@ function formatTestType ($test_type){
 		Return "Two-Way ANOVA";
 		break;
 	case 'three_way_rm_anova':
-		Return "Three-Way rmANOVA";
+		Return "Three-Way ANOVA with Repeated Measures";
 		break;
 	}
 }
@@ -94,38 +100,51 @@ function formatGroupNames ($group){
 //----------------------------------------------------------------------------------------
 //Print Output
 //----------------------------------------------------------------------------------------
-
-	if (!$result){
+if (!$result){
 		die("Query Failed");
 		}
 	else {
 		while($row = mysqli_fetch_array($result)) {
+			
 			echo "<br>".
 			"<h3><center>Search Results</center></h3>".
 			"<br>".
 			"<hr>".
 			"<b>Experiment Details</b>".
 			"<hr><br>".
-
 			"<table width = 50%>".
-			"<tr width=30%><td><b>Experiment: </td>".
-    			"<td>".$experiment."</td></tr>".
-			"<tr><td><b>Treatment Group 1: </td>".
-			     "<td>".formatGroupNames($group_1)."</td></tr>".
-			"<tr><td><b>Treatment Group 2: </td>".
-			     "<td>".formatGroupNames($group_2)."</td></tr>".
-	        	"<tr><td><b>Test Type </td>".
-	        		"<td>".formatTestType($row["test_type"])."</td></tr>".
+			"<tr><td width=30%><b>Experiment: </td>".
+    				"<td>".$experiment."</td></tr>".
+			"<tr><td><b>Test Type </td>".
+				"<td>".formatTestType($row["test_type"])."</td></tr>".
+			"</table><br>".
+			"<table width = 50%>".
+			"<tr><td width = 10%><b>Treatment Group</td>".
+				"<td width = 5%><b>Mean</b></td>".
+				"<td width = 5%><b>SE</b></td>".
+				"<td width = 5%><b>n</b></td>".
+			"</tr>".
+			"<tr><td>".formatGroupNames($group_1)."</td>".
+				"<td>".$row["total_mean_1"]."</td>".
+				"<td>".$row["total_se_1"]."</td>".
+				"<td>".$row["n_1"]."</td>".
+			"</tr>".
+			"<tr>".
+			     "<td>".formatGroupNames($group_2)."</td>".
+				"<td>".$row["total_mean_2"]."</td>".
+				"<td>".$row["total_se_2"]."</td>".
+				"<td>".$row["n_2"]."</td>".
+		 "</tr>".
 			"</table><br><br>".
 
 			"<hr><b>Main Effects</b><hr><br>".
 
-			"<table width = 70%>".
-			"<tr><td width = 30%><b>Main Effect</center></td>".
-				"<td><b>Error</td>".
-				"<td><b>df</td>".
-				"<td><b>F</td>".
-				"<td><b>P</td></tr>".
+			"<table width = 50%>".
+			"<tr><td width = 10%><b>Main Effect</center></td>".
+				"<td width = 5%><b>Error</td>".
+				"<td width = 5%><b>df</td>".
+				"<td width = 5%><b>F</td>".
+				"<td width = 5%><b>P</td></tr>".
 			"<tr><td>Exposure + Treatment</center></td>".
 				"<td>".$row["me_exposure_err"]."</td>".
 				"<td>".$row["me_exposure_df"]."</td>".
@@ -149,99 +168,83 @@ function formatGroupNames ($group){
 			"<tr><td width = 10%><b>Region</td>".
 				"<td width = 10%><b>Group 1 Mean</td>".
 				"<td width = 10%><b>Group 1 STE</td>".
-				"<td width = 10%><b>Group 1 n</td>".
 				"<td width = 10%><b>Group 2 Mean</td>".
 				"<td width = 10%><b>Group 2 STE</td>".
-				"<td width = 10%><b>Group 2 n</td>".
 				"<td width = 10%><b>P Value</td></tr>".
 			"<tr><td>0 uA</td>".
 				"<td>".$row["mean_0_1"]."</td>".
 				"<td>".$row["se_0_1"]."</td>".
-				"<td>".$row["n_0_1"]."</td>".
 				"<td>".$row["mean_0_2"]."</td>".
 				"<td>".$row["se_0_2"]."</td>".
-				"<td>".$row["n_0_2"]."</td>".
 				"<td>".$row["p_0"]."</td></tr>".
 			"<tr><td>50 uA</td>".
 				"<td>".$row["mean_50_1"]."</td>".
 				"<td>".$row["se_50_1"]."</td>".
-				"<td>".$row["n_50_1"]."</td>".
 				"<td>".$row["mean_50_2"]."</td>".
 				"<td>".$row["se_50_2"]."</td>".
-				"<td>".$row["n_50_2"]."</td>".
 				"<td>".$row["p_50"]."</td></tr>".
 			"<tr><td>100 uA</td>".
 				"<td>".$row["mean_100_1"]."</td>".
 				"<td>".$row["se_100_1"]."</td>".
-				"<td>".$row["n_100_1"]."</td>".
 				"<td>".$row["mean_100_2"]."</td>".
 				"<td>".$row["se_100_2"]."</td>".
-				"<td>".$row["n_100_2"]."</td>".
 				"<td>".$row["p_100"]."</td></tr>".
 			"<tr><td>150 uA</td>".
 				"<td>".$row["mean_150_1"]."</td>".
 				"<td>".$row["se_150_1"]."</td>".
-				"<td>".$row["n_150_1"]."</td>".
 				"<td>".$row["mean_150_2"]."</td>".
 				"<td>".$row["se_150_2"]."</td>".
-				"<td>".$row["n_150_2"]."</td>".
 				"<td>".$row["p_150"]."</td></tr>".
 			"<tr><td>200 uA</td>".
 				"<td>".$row["mean_200_1"]."</td>".
 				"<td>".$row["se_200_1"]."</td>".
-				"<td>".$row["n_200_1"]."</td>".
 				"<td>".$row["mean_200_2"]."</td>".
 				"<td>".$row["se_200_2"]."</td>".
-				"<td>".$row["n_100_2"]."</td>".
 				"<td>".$row["p_200"]."</td></tr>".
 			"<tr><td>250 uA</td>".
 				"<td>".$row["mean_250_1"]."</td>".
 				"<td>".$row["se_250_1"]."</td>".
-				"<td>".$row["n_250_1"]."</td>".
 				"<td>".$row["mean_250_2"]."</td>".
 				"<td>".$row["se_250_2"]."</td>".
-				"<td>".$row["n_150_2"]."</td>".
 				"<td>".$row["p_250"]."</td></tr>".
 			"<tr><td>300 uA</td>".
 				"<td>".$row["mean_300_1"]."</td>".
 				"<td>".$row["se_300_1"]."</td>".
-				"<td>".$row["n_300_1"]."</td>".
 				"<td>".$row["mean_300_2"]."</td>".
 				"<td>".$row["se_300_2"]."</td>".
-				"<td>".$row["n_300_2"]."</td>".
 				"<td>".$row["p_300"]."</td></tr>".
 			"<tr><td>350 uA</td>".
 				"<td>".$row["mean_350_1"]."</td>".
 				"<td>".$row["se_350_1"]."</td>".
-				"<td>".$row["n_350_1"]."</td>".
 				"<td>".$row["mean_350_2"]."</td>".
 				"<td>".$row["se_350_2"]."</td>".
-				"<td>".$row["n_350_2"]."</td>".
 				"<td>".$row["p_350"]."</td></tr>".
-			"</table>";
-			
+			"</table><br>";
+
 //----------------------------------------------------------------------------------------
 //Print for publication
 //----------------------------------------------------------------------------------------
-
-			echo "<br>".
-			"<h3><center>Search Results</center></h3>".
-			"<br>".
-			"<hr>".
+			echo "<hr>".
 			"<b>Formatted for inline citations</b>".
 			"<hr><br>".
 			"(".formatTestType($row["test_type"]).": Group: F<sub>(".$row["me_exposure_err"].
 			", ".$row["me_exposure_df"].")</sub> = ".$row["me_exposure_f"].", ".
 			"<i>P</i> = ".$row["me_exposure_p"]. "; ".
-			"Brain Region: F<sub>(".$row["me_intensity_err"].
+			"Intensity: F<sub>(".$row["me_intensity_err"].
 			", ".$row["me_intensity_df"].")</sub> = ".$row["me_intensity_f"].", ".
 			"<i>P</i> = ".$row["me_intensity_p"]."; ".
-			"Brain Region: F<sub>(".$row["me_interaction_err"].
+			"Interaction: F<sub>(".$row["me_interaction_err"].
 			", ".$row["me_interaction_df"].")</sub> = ".$row["me_interaction_f"].", ".
-			"<i>P</i> = ".$row["me_interaction_p"].")";
+			"<i>P</i> = ".$row["me_interaction_p"].
+			". ".formatGroupNames($group_1)." : <i>n</i> = ".$row["n_1"].
+			", ".formatGroupNames($group_2).": <i>n</i> = ". $row["n_2"].")";
 
 		}
 	}
+
+
+
+
 
 
 
