c34396699d9fac0e6e0cf913bb59e987a8a6242a
Haley Speed <haleygeek@users.noreply.github.com>
2017-07-05 23:11:27 -0500

    Update addache.php

--- addache.php
+++ addache.php
@@ -11,10 +11,11 @@
 //Connection info
 //----------------------------------------------------------------------------------------
 $dbhost = "localhost";
-$dbuser = "username"; 		//Edit this to make it work
-$dbpass = "userpassword";	//Edit this to make it work
-$dbname = "db_stats";		//Edit this to make it work
+$dbuser = "replace";
+$dbpass = "replace";
+$dbname = "replace";
 $connection = mysqli_connect($dbhost, $dbuser, $dbpass, $dbname);
+
 
 //----------------------------------------------------------------------------------------
 // Get variable assignments from form
@@ -23,55 +24,64 @@ if ($_SERVER["REQUEST_METHOD"] == "POST"){
 	$group_1 = $_POST["group_1"];
 	$group_2 = $_POST["group_2"];
 	$test_type = $_POST["test_type"];
+	$experiment = 'Acetylcholinesterase Assays';
+	
+	$total_mean_1 = test_input($_POST["total_mean_1"]);
+	$total_se_1 = test_input($_POST["total_se_1"]);
+	$n_1 = test_input($_POST["n_1"]);
+	$total_mean_2 = test_input($_POST["total_mean_2"]);
+	$total_se_2 = test_input($_POST["total_se_2"]);
+	$n_2 = test_input($_POST["n_2"]);
 
-	$me_exposure_err = $_POST["me_exposure_err"];
-	$me_exposure_df = $_POST["me_exposure_df"];
-	$me_exposure_f = $_POST["me_exposure_f"];
-	$me_exposure_p = $_POST["me_exposure_p"];
+	$me_exposure_err = test_input($_POST["me_exposure_err"]);
+	$me_exposure_df = test_input($_POST["me_exposure_df"]);
+	$me_exposure_f = test_input($_POST["me_exposure_f"]);
+	$me_exposure_p =test_input( $_POST["me_exposure_p"]);
 
-	$me_region_err = $_POST["me_region_err"];
-	$me_region_df = $_POST["me_region_df"];
-	$me_region_f = $_POST["me_region_f"];
-	$me_region_p = $_POST["me_region_p"];
+	$me_region_err = test_input($_POST["me_region_err"]);
+	$me_region_df = test_input($_POST["me_region_df"]);
+	$me_region_f = test_input($_POST["me_region_f"]);
+	$me_region_p = test_input($_POST["me_region_p"]);
 
-	$me_interaction_err = $_POST["me_interaction_err"];
-	$me_interaction_df = $_POST["me_interaction_df"];
-	$me_interaction_f = $_POST["me_interaction_f"];
-	$me_interaction_p = $_POST["me_interaction_p"];
+	$me_interaction_err = test_input($_POST["me_interaction_err"]);
+	$me_interaction_df = test_input($_POST["me_interaction_df"]);
+	$me_interaction_f = test_input($_POST["me_interaction_f"]);
+	$me_interaction_p = test_input($_POST["me_interaction_p"]);
 
-	$hipp_mean_1 = $_POST["hipp_mean_1"];
-	$hipp_se_1 = $_POST["hipp_se_1"];
-	$hipp_n_1 = $_POST["hipp_n_1"];
-	$hipp_mean_2 = $_POST["hipp_mean_2"];
-	$hipp_se_2 = $_POST["hipp_se_2"];
-	$hipp_n_2 = $_POST["hipp_n_2"];
-	$hipp_p = $_POST["hipp_p"];
+	$hipp_mean_1 = test_input($_POST["hipp_mean_1"]);
+	$hipp_se_1 = test_input($_POST["hipp_se_1"]);
+	$hipp_mean_2 =test_input( $_POST["hipp_mean_2"]);
+	$hipp_se_2 = test_input($_POST["hipp_se_2"]);
+	$hipp_p = test_input($_POST["hipp_p"]);
 
-	$cort_mean_1 = $_POST["cort_mean_1"];
-	$cort_se_1 = $_POST["cort_se_1"];
-	$cort_n_1 = $_POST["cort_n_1"];
-	$cort_mean_2 = $_POST["cort_mean_2"];
-	$cort_se_2 = $_POST["cort_se_2"];
-	$cort_n_2 = $_POST["cort_n_2"];
-	$cort_p = $_POST["cort_p"];
+	$cort_mean_1 = test_input($_POST["cort_mean_1"]);
+	$cort_se_1 = test_input($_POST["cort_se_1"]);
+	$cort_mean_2 = test_input($_POST["cort_mean_2"]);
+	$cort_se_2 = test_input($_POST["cort_se_2"]);
+	$cort_p = test_input($_POST["cort_p"]);
 
-	$cerb_mean_1 = $_POST["cerb_mean_1"];
-	$cerb_se_1 = $_POST["cerb_se_1"];
-	$cerb_n_1 = $_POST["cerb_n_1"];
-	$cerb_mean_2 = $_POST["cerb_mean_2"];
-	$cerb_se_2 = $_POST["cerb_se_2"];
-	$cerb_n_2 = $_POST["cerb_n_2"];
-	$cerb_p = $_POST["cerb_p"];
+	$cerb_mean_1 = test_input($_POST["cerb_mean_1"]);
+	$cerb_se_1 = test_input($_POST["cerb_se_1"]);
+	$cerb_mean_2 = test_input($_POST["cerb_mean_2"]);
+	$cerb_se_2 = test_input($_POST["cerb_se_2"]);
+	$cerb_p = test_input($_POST["cerb_p"]);
 
-	$striat_mean_1 = $_POST["striat_mean_1"];
-	$striat_se_1 = $_POST["striat_se_1"];
-	$striat_n_1 = $_POST["striat_n_1"];
-	$striat_mean_2 = $_POST["striat_mean_2"];
-	$striat_se_2 = $_POST["striat_se_2"];
-	$striat_n_2 = $_POST["striat_n_2"];
-	$striat_p = $_POST["striat_p"];
+	$striat_mean_1 = test_input($_POST["striat_mean_1"]);
+	$striat_se_1 = test_input($_POST["striat_se_1"]);
+	$striat_mean_2 = test_input($_POST["striat_mean_2"]);
+	$striat_se_2 = test_input($_POST["striat_se_2"]);
+	$striat_p = test_input($_POST["striat_p"]);
 }
 
+//----------------------------------------------------------------------------------------
+//Test Input
+//----------------------------------------------------------------------------------------
+function test_input($data){
+	$data = trim($data);
+	$data = stripslashes($data);
+	$data = strtolower($data);
+	Return $data;
+}
 
 //----------------------------------------------------------------------------------------
 //Line Up Groups
@@ -115,11 +125,56 @@ switch($group_1){
 		break;
 }
 
-
 //----------------------------------------------------------------------------------------
-//Update database (see db_stats.sql for example database)
+//Update database
 //----------------------------------------------------------------------------------------
-$query = "UPDATE ache SET test_type = '$test_type', me_exposure_f = '$me_exposure_f', me_exposure_err = '$me_exposure_err', me_exposure_df = '$me_exposure_df', me_exposure_p = '$me_exposure_p', me_region_f = '$me_region_f', me_region_err = '$me_region_err', me_region_df = '$me_region_df', me_region_p = '$me_region_p', me_interaction_f = '$me_interaction_f', me_interaction_err = '$me_interaction_err', me_interaction_df = '$me_interaction_df', me_interaction_p = '$me_interaction_p', hipp_mean_1 = '$hipp_mean_1', hipp_se_1 = '$hipp_se_1', hipp_n_1 = '$hipp_n_1', hipp_mean_2 = '$hipp_mean_2', hipp_se_2 = '$hipp_se_2', hipp_n_2 = '$hipp_n_2', hipp_p = '$hipp_p', cort_mean_1 = '$cort_mean_1', cort_se_1 = '$cort_se_1', cort_n_1 = '$cort_n_1', cort_mean_2 = '$cort_mean_2', cort_se_2 = '$cort_se_2', cort_n_2 = '$cort_n_2', cort_p = '$cort_p', cerb_mean_1 = '$cerb_mean_1', cerb_se_1 = '$cerb_se_1', cerb_n_1 = '$cerb_n_1', cerb_mean_2 = '$cerb_mean_2', cerb_se_2 = '$cerb_se_2', cerb_n_2 = '$cerb_n_2', cerb_p = '$cerb_p', striat_mean_1 = '$striat_mean_1', striat_se_1 = '$striat_se_1', striat_n_1 = '$striat_n_1', striat_mean_2 = '$striat_mean_2', striat_se_2 = '$striat_se_2', striat_n_2 = '$striat_n_2', striat_p = '$striat_p' WHERE group_1 = '$group_1' AND group_2 = '$group_2'";
+$query = "UPDATE ache SET test_type = '$test_type',".
+	
+	" total_mean_1 = '$total_mean_1',".
+	" total_se_1 = '$total_se_1',".
+	" n_1 = '$n_1',".
+	" total_mean_2 = '$total_mean_2',".
+	" total_se_2 = '$total_se_2',".
+	" n_2 = '$n_2',".
+	
+	" me_exposure_f = '$me_exposure_f',". 
+	" me_exposure_err = '$me_exposure_err',". 
+	" me_exposure_df = '$me_exposure_df',". 
+	" me_exposure_p = '$me_exposure_p',".
+	" me_region_f = '$me_region_f',".
+	" me_region_err = '$me_region_err',".
+	" me_region_df = '$me_region_df',".
+	" me_region_p = '$me_region_p',".
+	" me_interaction_f = '$me_interaction_f',". 
+	" me_interaction_err = '$me_interaction_err',".
+	" me_interaction_df = '$me_interaction_df',".
+	" me_interaction_p = '$me_interaction_p',".
+	
+	" hipp_mean_1 = '$hipp_mean_1',".
+	" hipp_se_1 = '$hipp_se_1',".
+	" hipp_mean_2 = '$hipp_mean_2',".
+	" hipp_se_2 = '$hipp_se_2',".
+	" hipp_p = '$hipp_p',".
+	
+	" cort_mean_1 = '$cort_mean_1',".
+	" cort_se_1 = '$cort_se_1',".
+	" cort_mean_2 = '$cort_mean_2',".
+	" cort_se_2 = '$cort_se_2',".
+	" cort_p = '$cort_p',".
+	
+	" cerb_mean_1 = '$cerb_mean_1',".
+	" cerb_se_1 = '$cerb_se_1',".
+	" cerb_mean_2 = '$cerb_mean_2',".
+	" cerb_se_2 = '$cerb_se_2',".
+	" cerb_p = '$cerb_p',".
+	
+	" striat_mean_1 = '$striat_mean_1',".
+	" striat_se_1 = '$striat_se_1',".
+	" striat_mean_2 = '$striat_mean_2',".
+	" striat_se_2 = '$striat_se_2',".
+	" striat_p = '$striat_p' ".
+	
+	"WHERE group_1 = '$group_1' AND group_2 = '$group_2'";
 
 $result = mysqli_query($connection, $query);
 
